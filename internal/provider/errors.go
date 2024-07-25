2f581bdd9391652e0cfafd5e45edf6b2b646d1c8
Kim Oliver Drechsel <kimoliver.drechsel@adorsys.com>
2024-07-25 10:36:43 +0200

    lint

--- internal/provider/errors.go
+++ internal/provider/errors.go
@@ -2,5 +2,7 @@ package provider
 
 import "errors"
 
-var ErrFileNotFound = errors.New("no such file or directory")
-var ErrInvalidLine = errors.New("invalid line")
+var (
+	ErrFileNotFound = errors.New("no such file or directory")
+	ErrInvalidLine  = errors.New("invalid line")
+)
